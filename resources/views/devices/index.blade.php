86f195ca27586ae0cefb74ca2f2403742180f99a
sovandara1607 <srith3@paragoniu.edu.kh>
2026-02-19 15:19:07 +0700

    msg

--- resources/views/devices/index.blade.php
+++ resources/views/devices/index.blade.php
@@ -1,3 +1,3 @@
-<table>
-   device list
-</table>
+This is the device list page. You can customize this view to display the devices in a way that suits your needs. For
+example, you might want to display the device name, type, status, and any other relevant information. You can also add
+actions such as edit or delete for each device.
